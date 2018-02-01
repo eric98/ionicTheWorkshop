0e2ea751a72fb9dca8547797cdc0573ab7f741f5
eric981 <jpqsk27r>
2018-02-01 12:55:49 +0100

    LoginPage done

---
+++ src/pages/login/login.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {HomePage} from "../home/home";
+
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  user: string;
+  pass: string;
+  isLogged: boolean;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+  ) {
+  }
+
+  Signup() {
+    let f = {user: this.user, pass: this.pass}
+    if(f.user == 'user' && f.pass == 'password'){
+      this.isLogged = true
+    }
+    if (this.isLogged) {
+      this.navCtrl.setRoot(HomePage)
+        .then(data => console.log(data),
+          error => console.log(error))
+    } else {
+      console.log('Acces denegat')
+    }
+  }
+
+}
